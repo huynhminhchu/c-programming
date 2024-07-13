a5b9544722704b12e09b030e4d723e84e68441ca
Chu Huynh <huynhminhchu1999@gmail.com>
2024-07-13 02:18:19 +0000

    test

--- csapp/ecf/process-control/waitpid1.c
+++ csapp/ecf/process-control/waitpid1.c
@@ -1,16 +1,33 @@
-#include "csapp.h"
-
+#include <dbg.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #define N 2
 
-int main()
-{
-    int status, i;
-    pid_t pid;
+int main() {
+  int status, i;
+  pid_t pid;
 
-    for (i = 0; i < N; i++) {
-        if ( (pid = Fork()) == 0) // if this is child process
-            exit(100+i);
-    }
+  for (i = 0; i < N; i++) {
+    pid = fork();
+    check(pid >= 0, "fork failed.");
+    if (pid == 0) // child
+      exit(100 + i);
+  }
 
-    while 
+  // Parent reaps N children in no particular oder
+  while ((pid = waitpid(-1, &status, 0)) > 0) {
+    if (WIFEXITED(status))
+      log_info("child %d terminated normally with exit status =%d", pid,
+               WEXITSTATUS(status));
+    else
+      log_info("child %d terminated abnormally.", pid);
+  }
+
+  if (errno != ECHILD)
+    sentinel("waitpid error");
+error:
+  return -1;
 }
